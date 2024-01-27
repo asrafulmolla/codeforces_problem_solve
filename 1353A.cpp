a3c6044b520abe952be9a82d90676f31a9172066
Md. Asraful Molla <128937137+asrafulmolla@users.noreply.github.com>
2024-01-27 23:23:56 +0600

    Add files via upload

---
+++ 1353A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+	long long int t,a,i,n,m,j,x,y,c,z;
+	string s;
+	scanf("%lld",&t);
+	while(t--)
+	{
+		cin>>n>>m;
+          if(n==1)
+            cout<<0<<endl;
+          else if(n==2)
+            cout<<m<<endl;
+          else
+            cout<<m*2<<endl;
+		}
+}
+
